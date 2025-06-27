6a12c12b0bd4e559a0ee3a2fdbd0217482676e0d
LavanyaKarre <229x1a3311@gmail.com>
2025-06-27 14:40:23 +0530

    Create Calculator.java

---
+++ JUnit, Mockito and SL4J/JUnit_Basic Testing Exercises/Setting Up Junit/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    /*Adds two integers. */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /* Multiplies two integers. */
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
