cf264d8ca36482c5b12556ce6ce8dbbc50d1b3e2
LavanyaKarre <229x1a3311@gmail.com>
2025-06-27 14:21:32 +0530

    Create highBalance.sql

---
+++ PL SQL Programming/Control Structures/highBalance.sql
@@ -0,0 +1,8 @@
+BEGIN
+  FOR cust IN (SELECT customer_id FROM customers WHERE balance > 10000) LOOP
+    UPDATE customers
+    SET isVIP = 'TRUE'
+    WHERE customer_id = cust.customer_id;
+  END LOOP;
+  COMMIT;
+END;
